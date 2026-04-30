21ffac8e2659f1851f7fca37ac94f7ae412bcc6f
shayanabdullah <tuhinafroza07@gmail.com>
2026-05-01 01:19:28 +0600

    hero done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,45 +11,70 @@ import { IoArrowBackOutline, IoArrowForwardOutline } from "react-icons/io5";
 const Hero = () => {
   return (
     <section>
- <Container>
-       <div className="relative w-full">
-        <Swiper
-          modules={[Navigation, Pagination]}
-          navigation={{
-            prevEl: '.custom-prev',
-            nextEl: '.custom-next',
-          }}
-          pagination={{ clickable: true, el: '.custom-pagination' }}
-          loop={true}
-          className="size-full"
-        >
-          <SwiperSlide>
-            <img src={heroOne} className="w-full h-full " alt="slide 1" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src={heroTwo} className="w-full min-h-137.75 object-cover" alt="slide 1" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src={heroThree} className="w-full min-h-137.75 object-cover" alt="slide 1" />
-          </SwiperSlide>
-        </Swiper>
+      <Container>
+        <div className="relative w-full px-6 md:px-0">
+          <Swiper
+            modules={[Navigation, Pagination, Autoplay]}
+            navigation={{
+              prevEl: ".custom-prev",
+              nextEl: ".custom-next",
+            }}
+            pagination={{ clickable: true, el: ".custom-pagination" }}
+            loop={true}
+            autoplay={{ delay: 3000 }}
+            className="size-full"
+          >
+            <SwiperSlide>
+              <img
+                src={heroOne}
+                className="w-full min-h-76 md:min-h-full "
+                alt="slide 1"
+              />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img
+                src={heroTwo}
+                className="w-full min-h-76 md:min-h-full object-cover"
+                alt="slide 2"
+              />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img
+                src={heroThree}
+                className="w-full min-h-76 md:min-h-full  object-cover"
+                alt="slide 3"
+              />
+            </SwiperSlide>
+          </Swiper>
 
-        {/* Custom Prev Button */}
-        <button className="custom-prev absolute left-3 top-1/2 -translate-y-1/2 z-10 w-10 h-10 rounded-full bg-white shadow flex items-center justify-center hover:bg-gray-100 transition cursor-pointer text-2xl">
-          <IoArrowBackOutline  />
-        </button>
+          {/* Custom Prev Button */}
+          <button className="custom-prev absolute left-3 top-1/2 -translate-y-1/2 z-10 w-10 h-10 rounded-full bg-white shadow hidden md:flex items-center justify-center hover:bg-gray-100 transition cursor-pointer text-2xl ">
+            <IoArrowBackOutline />
+          </button>
 
-        {/* Custom Next Button */}
-        <button className="custom-next absolute right-3 top-1/2 -translate-y-1/2 z-10 w-10 h-10 rounded-full bg-white shadow flex items-center justify-center hover:bg-gray-100 transition cursor-pointer text-2xl">
-          <IoArrowForwardOutline />
-        </button>
+          {/* Custom Next Button */}
+          <button className="custom-next absolute right-3 top-1/2 -translate-y-1/2 z-10 w-10 h-10 rounded-full bg-white shadow  items-center justify-center hover:bg-gray-100 transition cursor-pointer text-2xl hidden md:flex">
+            <IoArrowForwardOutline />
+          </button>
 
-        {/* Custom Pagination Dots */}
-        <div className="custom-pagination absolute bottom-4 left-1/2 -translate-x-1/2 z-10 flex gap-2" />
-      </div>
- </Container>
+          {/* Custom Pagination Dots */}
+          <div className="custom-pagination absolute bottom-4 left-1/2 -translate-x-1/2 z-10 flex gap-2" />
+        </div>
+
+        {/* Text */}
+        <div className="py-10 px-4 md:px-0">
+          <div className="main grid grid-cols-1 md:grid-cols-2 gap-y-4 items-center">
+            <div className="right">
+              <h1 className="font-poppins font-semibold text-[40px] md:text-[72px] leading-[150%]">Simply Unique<span className="text-neutral-4">/</span> Simply Better.</h1>
+            </div>
+            <div className="left">
+              <p className="font-inter font-normal text-base text-neutral-4"><span className="font-semibold text-neutral-5">3legant</span> is a gift & decorations store based in HCMC, Vietnam. Est since 2019. </p>
+            </div>
+          </div>
+        </div>
+      </Container>
     </section>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,28 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import Container from "./layouts/Container";
 import ticket from "../assets/icons/ticket-percent.webp";
 import { Link } from "react-router";
 import { IoArrowForward, IoClose } from "react-icons/io5";
-import { motion } from "motion/react"
+import { motion } from "motion/react";
 import { fadeIn, textVariant } from "../utils/animations";
+
 const TopBar = () => {
+  const [topbar, setTopbar] = useState(true);
+  const handleClick = () => {
+    setTopbar(false);
+  };
   return (
     <>
-      <motion.header variants={fadeIn('down', 0.1)} animate='show' initial='hidden' className="py-2.5 bg-neutral-2 flex justify-end items-center px-6">
-        <Container>
-          <div className="main  flex items-center justify-center gap-x-3 w-fu">
-            <img src={ticket} alt="icon" fetchPriority="high" loading="lazy"/>
-            <motion.h2 variants={textVariant(0.1)} className="font-inter font-semibold text-sm text-neutral-5">
-              30% off storewide — Limited time!
-            </motion.h2>
-            <Link className="flex items-center gap-x-1 text-blue font-inter font-medium text-sm h-full border-b border-blue">
-            Shop Now
-            <IoArrowForward />
-            </Link>
+      {topbar && (
+        <motion.header
+          variants={fadeIn("down", 0.1)}
+          animate="show"
+          initial="hidden"  
+          className="py-2.5 bg-neutral-2 flex justify-end items-center px-6"
+
+        >
+          <Container>
+            <div className="main  flex items-center justify-center gap-x-3 w-full">
+              <img
+                src={ticket}
+                alt="icon"
+                fetchPriority="high"
+                loading="lazy"
+              />
+              <motion.h2
+                variants={textVariant(0.1)}
+                className="font-inter font-semibold text-xs md:text-md text-neutral-5"
+              >
+                30% off storewide — Limited time!
+              </motion.h2>
+              <Link className=" hidden md:flex items-center gap-x-1 text-blue font-inter font-medium text-xs md:text-md h-full border-b border-blue">
+                Shop Now
+                <IoArrowForward />
+              </Link>
+            </div>
+          </Container>
+          <div
+            className="cross text-2xl text-neutral-5 cursor-pointer"
+            onClick={handleClick}
+          >
+            <IoClose />
           </div>
-        </Container>
-        <div className="cross text-2xl text-neutral-5 cursor-pointer"><IoClose /></div>
-      </motion.header>
+        </motion.header>
+      )}
     </>
   );
 };
