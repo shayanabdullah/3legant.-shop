c37183b68b5747a2c45513ef63f52888e4cee2a4
shayanabdullah <tuhinafroza07@gmail.com>
2026-05-03 00:07:37 +0600

    store feature section done

--- src/components/StoreFeatures.jsx
+++ src/components/StoreFeatures.jsx
@@ -4,6 +4,8 @@ import { FiTruck } from 'react-icons/fi'
 import { PiCreditCard } from "react-icons/pi";
 import { CiLock } from "react-icons/ci";
 import { LuPhone } from "react-icons/lu";
+import { motion } from 'motion/react';
+import { fadeIn, textVariant } from './../utils/animations';
 const StoreFeatures = () => {
 
 const valueProps = [
@@ -12,24 +14,28 @@ const valueProps = [
     title: "Free Shipping",
     description: "Order above $200",
     icon: FiTruck,
+    delay : 0.1
   },
   {
     id: 2,
     title: "Money-back",
     description: "30 days guarantee",
     icon: PiCreditCard,
+     delay : 0.2
   },
   {
     id: 3,
     title: "Secure Payments",
     description: "Secured by Stripe",
     icon: CiLock,
+     delay : 0.3
   },
   {
     id: 4,
     title: "24/7 Support",
     description: "Phone and Email support",
     icon: LuPhone,
+     delay : 0.4
   },
 ];
   return (
@@ -38,13 +44,13 @@ const valueProps = [
             <div className="grid grid-cols-2 md:grid-cols-4 gap-x-2 gap-y-6 md:gap-x-6 ">
               {
                 valueProps.map((item)=> (
-                    <div className="py-8 md:py-12 px-4 md:px-8  bg-neutral-2" key={item.id}>
+                    <motion.div variants={fadeIn('up', item.delay)} initial='hidden' animate="show" viewport={{once:true}} className="py-8 md:py-12 px-4 md:px-8  bg-neutral-2" key={item.id}>
                     <item.icon className='text-4xl mb-4'/>
                     <div className="">
-                        <h2 className='font-poppins  text-sm md:text-xl font-semibold text-neutral-7 pb-2'>{item.title}</h2>
-                        <p className='font-inter text-sm font-normal text-neutral-4'>{item.description}</p>
+                        <motion.h2 variants={textVariant(item.delay)}  viewport={{once:true}} className='font-poppins  text-sm md:text-xl font-semibold text-neutral-7 pb-2'>{item.title}</motion.h2>
+                        <motion.p variants={textVariant(item.delay + 0.1)}  viewport={{once:true}} className='font-inter text-sm font-normal text-neutral-4'>{item.description}</motion.p>
                     </div>
-                </div>
+                </motion.div>
                 ))
               }
             </div>
